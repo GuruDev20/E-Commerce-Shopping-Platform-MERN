fa04f938accef68da36460ec6193515ba701592c
Guru Dev <guru01803@gmail.com>
2024-01-09 23:17:00 +0530

    added multi carousel to men

--- src/components/views/Men.js
+++ src/components/views/Men.js
@@ -1,17 +1,59 @@
-import React from 'react'
-import '../../styles/Men.css';
-import Men from '../../assets/men1.jpg'
+import React from "react";
+import "../../styles/Men.css";
+import Men from "../../assets/men1.jpg";
+import Carousel from "react-multi-carousel";
+import "react-multi-carousel/lib/styles.css";
+
 function view2() {
+  const responsive = {
+    superLargeDesktop: {
+      breakpoint: { max: 4000, min: 3000 },
+      items: 5,
+    },
+    desktop: {
+      breakpoint: { max: 3000, min: 1024 },
+      items: 3,
+    },
+    tablet: {
+      breakpoint: { max: 1024, min: 464 },
+      items: 2,
+    },
+    mobile: {
+      breakpoint: { max: 464, min: 0 },
+      items: 1,
+    },
+  };
+
   return (
-    <div>
-      <div className='mens-coll'>
-        <div className='men-left rev'>Men's Collection</div>
-        <div className='men-right reveal'>
-          <img src={Men} alt="men" className='men-img'/>
-        </div>
+    <div className="mens-coll">
+      <div className="men-left rev">Men's Collection</div>
+      <div className="men-right reveal">
+        <img src={Men} alt="men" className="men-img" />
+      </div>
+      <div className="carousel-container">
+        <Carousel responsive={responsive}>
+          <div className="cards">
+            <img src={Men} alt="" className="card-img" />
+          </div>
+          <div className="cards">
+            <img src={Men} alt="" className="card-img" />
+          </div>
+          <div className="cards">
+            <img src={Men} alt="" className="card-img" />
+          </div>
+          <div className="cards">
+            <img src={Men} alt="" className="card-img" />
+          </div>
+          <div className="cards">
+            <img src={Men} alt="" className="card-img" />
+          </div>
+          <div className="cards">
+            <img src={Men} alt="" className="card-img" />
+          </div>
+        </Carousel>
       </div>
     </div>
-  )
+  );
 }
 
-export default view2
+export default view2;
