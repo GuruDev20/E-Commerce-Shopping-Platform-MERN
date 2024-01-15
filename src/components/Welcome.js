56d380c3525a0dffd1a0545702461925e7ac2d85
Guru Dev <guru01803@gmail.com>
2024-01-15 21:49:42 +0530

    added new navbar component

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -1,9 +1,6 @@
 import React from 'react';
 import '../styles/Welcome.css'
-import { GiShoppingBag } from "react-icons/gi";
-import { FaShoppingCart } from "react-icons/fa";
-import { CiHeart } from "react-icons/ci";
-import { Link } from 'react-router-dom';
+import Navbar from './Navbar';
 import Main from './views/Main'
 import Men from './views/Men';
 import Women from './views/Women';
@@ -13,65 +10,7 @@ import Others from './views/Others';
 function Welcome() {
   return (
     <div>
-        <div className='header'>
-          <div className='icon'>
-            <GiShoppingBag size={50} />
-            <span>Shopify</span>
-          </div>
-          <div className='navbar'>
-            <ul>
-              <li>
-                <Link to="/" className='nav-link' reloadDocument>Shop</Link>
-              </li>
-              <li>
-                  <div className='dropdown'>
-                    <Link to="/cloths/men-items" className='nav-link'>Mens</Link>
-                    <Link to="/cloths/women-items" className='nav-link'>Womens</Link>
-                    <Link to="/cloths/kids-items" className='nav-link'>Kids</Link>
-                    <Link to="/cloths/men-accessories-items" className='nav-link'>Men's Accessories</Link>
-                    <Link to="/cloths/women-accessories-items" className='nav-link'>Women's Accessories</Link>
-                    <Link to="/cloths/beauty" className='nav-link'>Beauty Products</Link>
-                  </div>
-                  <div className='nav-link-items'>Fashion & Clothing</div>
-              </li>
-              <li>
-                <div className='dropdown'>
-                  <Link to='/foods/breakfast' className='nav-link'>Breakfast</Link>
-                  <Link to='/foods/lunch' className='nav-link'>Lunch</Link>
-                  <Link to='/foods/dinner' className='nav-link'>Dinner</Link>
-                  <Link to='/foods/hotdrinks' className='nav-link'>Hot Drinks</Link>
-                  <Link to='/foods/softdrinks' className='nav-link'>Soft Drinks</Link>
-                </div>
-                <div className='nav-link-items'>Foods & Drinks</div>
-              </li>
-              <li>
-                <div className='dropdown'>
-                  <Link to='/home/plants' className='nav-link'>Plants</Link>
-                  <Link to='/home/lights' className='nav-link'>Lights</Link>
-                  <Link to='/home/decoration' className='nav-link'>Decoration</Link>
-                </div>
-                <div className='nav-link-items'>Home & Garden</div>
-              </li>
-              <li>
-                <div className='dropdown'>
-                  <Link to='/health/skincare' className='nav-link'>Skincare</Link>
-                  <Link to='/health/moisture' className='nav-link'>Moisturisers</Link>
-                  <Link to='/health/dailyproductus' className='nav-link'>Daily Products</Link>
-                </div>
-                <div className='nav-link-items'>Health & Beauty</div>
-              </li>
-            </ul>
-          </div>
-          <div className='loginbutton'>
-            <Link to='/loginregister'  className='nav-link'><button className='login'>Login</button></Link>
-          </div>
-          <div className='whislist'>
-            <CiHeart size={40} color='#e56b6f'/>
-          </div>
-          <div className='cart'>
-            <FaShoppingCart size={30}/>
-          </div>
-        </div>
+        <div><Navbar/></div>
         <div><Main/></div>
         <div><Men/></div>
         <div><Women/></div>
--- src/components/utils/Dresses.js
+++ src/components/utils/Dresses.js
@@ -1,12 +1,27 @@
 import React from 'react'
 import '../../styles/Dresses.css'
+import Navbar from '../Navbar'
+import Footer from '../../components/views/Footer'
 function Dresses(props) {
   return (
     <div className='cloth-products'> 
-        <div className='cloths-product-container'>
-          <div className='cloths-filter'></div>
-          <div className='cloths-filter-content'></div>
+        <Navbar/>
+        <div className='cloths-search'>
+            <input type='search' placeholder='Search products' className='cloths-field-search'/>
         </div>
+        <div className='cloths-product-container'>
+          <div className='cloths-filter'>
+            Shop By
+            <div className='filter-by'></div>
+          </div>
+          <div className='cloths-filter-content'>
+            {props.sort}
+            <div className='sort-by'></div>
+            <div className='sort-results'></div>
+            <div className='more-cloths'></div>
+          </div>
+        </div>
+        <Footer/>
     </div>
   )
 }
